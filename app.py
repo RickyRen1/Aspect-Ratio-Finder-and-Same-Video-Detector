3782fd4b53e614b5554043daff0ea7573db806f6
RickyRen1 <rickyrenx@gmail.com>
2025-10-28 22:30:50 -0700

    update default port to 10000

--- app.py
+++ app.py
@@ -277,4 +277,4 @@ async def root():
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info", access_log=True)
+    uvicorn.run(app, host="0.0.0.0", port=10000, log_level="info", access_log=True)
